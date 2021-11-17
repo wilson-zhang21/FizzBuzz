b644a50b5ced2c9121424fb409cc975e9d72d6f5
wilson-zhang21 <wilson.zhang21@outlook.com>
2021-11-17 19:27:19 +0000

    part 2 and user input

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -6,27 +8,59 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.Out.WriteLine("Please enter the max number");
+            int max = Convert.ToInt32(Console.In.ReadLine());
             
             
-            for(int i = 1; i <= 100; i++ )
+            
+            for(int i = 1; i <= max; i++ )
             {
-                string res = "";
+                List<string> result = new List<string>();
                 if (i % 3 == 0 )
                 {
-                    res = res + "Fizz";
+                    result.Add("Fizz");
                 }
                 if (i % 5 == 0)
                 {
-                    res = res + "Buzz";
+                    result.Add("Buzz");
                 }
-                
-                if (res == "")
+
+                if (i % 7 == 0)
                 {
-                    Console.Out.WriteLine(i);
+                    result.Add("Bang");
+                }
+
+                if (i % 11 == 0)
+                {
+                    result.Add("Bong");
+                }
+
+                if (i % 13 == 0)
+                {
+                    int firstB = result.FindIndex(a => a.StartsWith("B"));
+                    if (firstB >= 0)
+                    {
+                        result.Insert(firstB, "Fezz");
+                    }
+                    else
+                    {
+                        result.Add("Fezz");
+                    }
+                }
+
+                if (i % 17 == 0)
+                {
+                    result.Reverse();
+                }
+
+
+                if (result.Any())
+                {
+                    Console.Out.WriteLine(string.Join("",result));
                 }
                 else
                 {
-                    Console.Out.WriteLine(res);
+                    Console.Out.WriteLine(i);
                 }
             }
         }
